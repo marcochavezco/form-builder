5a499d67da54211925c96d6cda6ba7d6eb0e0454
marcochavezco <machavezco@gmail.com>
2024-01-26 01:38:09 -0600

    Add dashboard layout, this includes Logo, ThemeSwitcher and UserButton compoents

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,15 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang='en'>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-        >
-          <body className={inter.className}>{children}</body>
-        </ThemeProvider>
+      <html lang='en' suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );

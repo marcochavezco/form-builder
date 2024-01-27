2241aa13ff3b48dde4906e6bf95e58f1d0e9d77c
marcochavezco <machavezco@gmail.com>
2024-01-26 19:26:53 -0600

    Add Stats Card using server actions and setup prisma client

---
+++ actions/form.ts
@@ -0,0 +1,35 @@
+'use server';
+
+import prisma from '@/lib/prisma';
+import { currentUser } from '@clerk/nextjs';
+
+class UserNotFoundErr extends Error {}
+export async function GetFormStats() {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  const stats = await prisma.form.aggregate({
+    where: {
+      userId: user.id,
+    },
+    _sum: {
+      visits: true,
+      submission: true,
+    },
+  });
+
+  const visits = stats?._sum.visits || 0;
+  const submissions = stats?._sum.submission || 0;
+
+  let submissionRate = 0;
+
+  if (visits > 0) {
+    submissionRate = (submissions / visits) * 100;
+  }
+
+  const bouceRate = 100 - submissionRate;
+
+  return { visits, submissions, submissionRate, bouceRate };
+}
